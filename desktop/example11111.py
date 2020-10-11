c00be23fbe66087ac8a1c6b117e751bb2a553f00
Vladislav <68337772+VladPetrov19@users.noreply.github.com>
2020-10-11 18:04:25 +0300

    Update example11111.py

--- desktop/example11111.py
+++ desktop/example11111.py
@@ -3,12 +3,32 @@ from dataclasses import dataclass
 
 @dataclass
 class DataReader:
+    file_path: str
 
-    def __init__(self, f=open('datafile.txt')):
-        self.f = f.read()
+    def __init__(self, file_path):
+        self.file_path = file_path
+        self.file = open(self.file_path)
+        self.is_end_of_file = False
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        string = ''
+        if self.is_end_of_file:
+            self.file.close()
+            raise StopIteration()
+        while True:
+            char = self.file.read(1)
+            self.is_end_of_file = char == ''
+            if char != ',' and not self.is_end_of_file:
+                string += char
+            else:
+                break
+        return float(string)
 
 
-reader = DataReader()
-r = reader.f
-for number in r:
+reader = DataReader('datafile.txt')
+
+for number in reader:
     print(number)
